ea889d9c1f216ead748c0cb57e3ce03a2ad0d727
Jay Gabriels <jgabriels30@gmail.com>
2020-11-04 16:41:43 -0800

    Update redis_set.py

--- Scripts/redis_set.py
+++ Scripts/redis_set.py
@@ -20,7 +20,7 @@ def redis_populate(filepath):
     """Function to populate keys in Redis Server"""
     configs = load_config(filepath)
     client = redis.StrictRedis(host=configs["redis_host"], port=configs["redis_port"], password=configs["redis_password"])
-    for i in range(100000):
+    for i in range(10000000):
         key='key'+str(i)
         value='value'+str(i)
         client.set(key,value)
